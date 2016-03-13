3b48c86c7dc77ba0848f7b898c1b1e8dc44c2240
Christian Ege <k4230r6@gmail.com>
2016-03-13 09:05:52 +0100

    Added MACHINE_FEATURE LVDS7
    This enables Qt5.x.x touch support for the KIT LCD 7" Touch device.
    To activate this feature add the following to your conf/local.conf
    
        MACHINE_FEATURES_append = " \
         lvds7 \
        "
    This can not be activate as general option because not everybody owns a
    7" touch screen panel from UDOO. A more sophisticated could be detecting
    the screen based on the current device tree and setting those values
    accordingly. This fixes #9
    
    Signed-off-by: Christian Ege <k4230r6@gmail.com>

--- qt5-layer/recipes-qt/qt5-env/files/qt5-env.sh
+++ qt5-layer/recipes-qt/qt5-env/files/qt5-env.sh
@@ -27,14 +27,6 @@ export PATH=${PATH}:/usr/bin/qt5
 #export QT_QPA_EGLFS_WIDTH=1024
 #export QT_QPA_EGLFS_HEIGHT=768
 
-# QT_QPA_EGLFS_PHYSICAL_WIDTH and QT_QPA_EGLFS_PHYSICAL_HEIGHT -
-# Physical screen width and height in millimeters. On platforms where
-# the framebuffer device /dev/fb0 is not available or the query is not
-# successful, the values are calculated based on a default DPI of 100.
-# This variable can be used to override any such defaults.
-#
-#export QT_QPA_EGLFS_PHYSICAL_WIDTH = 1024
-#export QT_QPA_EGLFS_PHYSICAL_WIDTH = 1024
 
 # QT_QPA_EGLFS_DEPTH - Overrides the color depth for the screen. On
 # platforms where the framebuffer device /dev/fb0 is not available or
